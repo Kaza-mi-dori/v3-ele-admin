121d54aa6ae457c5ac822460c689c90b5e375876
jhx <jiahaoxie7@outlook.com>
2024-11-03 23:10:31 +0800

    初始化

---
+++ src/components/CustomComponent/GeneralDataTable.vue
@@ -0,0 +1,83 @@
+<!-- 主要为对应台账管理，表头应该有筛选按钮，重点在列的定义 -->
+<template>
+  <div>
+    <!-- 标题 -->
+    <!-- 统计数据区 -->
+    <div class="title-block">
+      <div class="__title">标题</div>
+      <div class="__stat">
+        <span class="__item">统计指标一</span>
+        <span class="__item">统计指标二</span>
+      </div>
+    </div>
+    <!-- 筛选操作区 -->
+    <div class="filter-block">
+      <el-date-picker
+        v-model="date"
+        type="date"
+        placeholder="选择日期"
+        style="width: 200px"
+      />
+      <el-select
+        v-model="selectedItems"
+        multiple
+        placeholder="请选择"
+        style="width: 200px"
+      >
+        <el-option
+          v-for="item in selectableOptions"
+          :key="item"
+          :label="item"
+          :value="item"
+        />
+      </el-select>
+    </div>
+    <!-- 表格操作区 -->
+    <div class="op-block">
+      <el-button type="primary" @click="handleFilter">更多条件</el-button>
+    </div>
+    <!-- 表格区 -->
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+const date = ref("");
+const selectedItems = ref([]);
+const selectableOptions = ["选项一", "选项二", "选项三"];
+const handleFilter = () => {
+  console.log("更多条件");
+};
+</script>
+
+<style lang="scss" scoped>
+.title-block {
+  @apply m-1;
+  .__title {
+    @apply text-xl font-bold relative pl-3 mb-2;
+    &::after {
+      @apply block h-full w-1.25 bg-blue-5 absolute left-0 top-0;
+      content: "";
+    }
+  }
+  .__stat {
+    @apply flex pl-3;
+    .__item {
+      @apply m-1 text-sm text-gray-5;
+    }
+  }
+}
+
+.filter-block {
+  @apply flex m-1 items-center pl-3;
+  .el-date-picker,
+  .el-select {
+    @apply m-1;
+  }
+}
+
+.op-block {
+  @apply flex m-1 items-center justify-end;
+}
+</style>
---
+++ src/components/CustomComponent/SearchBar.vue
@@ -0,0 +1,179 @@
+<template>
+  <div>
+    <!-- 写成headless的纯逻辑抽象 -->
+    <!-- 筛选区 -->
+    <div>
+      <div class="active-filters-block">
+        <div v-for="item in selectedItems" :key="item.name" class="__item">
+          <span>{{ item.name }}:</span>
+          <div v-if="Array.isArray(item.selected)">
+            <span
+              v-for="option in item.selected"
+              :key="option"
+              class="closable-tag line-height-normal"
+            >
+              {{ option }}
+              <el-icon
+                name="close"
+                class="cursor-pointer color-red"
+                @click="handleSelect(item, option)"
+              >
+                <CircleClose />
+              </el-icon>
+            </span>
+          </div>
+          <span v-else>{{ item.selected }}</span>
+        </div>
+      </div>
+    </div>
+    <!-- 条件区 -->
+    <div>
+      <div
+        v-for="item in itemList as any"
+        :key="item.name"
+        class="condition-block"
+      >
+        <span class="__name">{{ item.name }}</span>
+        <div class="__options">
+          <span
+            v-for="option in item.options"
+            :key="option"
+            :class="{
+              __item: true,
+              active: Array.isArray(item.selected)
+                ? item.selected.includes(option)
+                : item.selected === option,
+            }"
+            @click="handleSelect(item, option)"
+          >
+            {{ option }}
+          </span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps, getCurrentInstance, nextTick } from "vue";
+
+const { ctx: that } = getCurrentInstance() as any;
+let selectedItems = ref();
+
+const props = defineProps({
+  itemList: {
+    type: Array,
+    required: true,
+    default: () => [],
+  },
+});
+
+const handleSelect = (item: any, option: any) => {
+  if (!item.multiple) {
+    if (item.selected === option) {
+      item.selected = null;
+    } else {
+      item.selected = option;
+    }
+  } else {
+    if (item.selected.includes(option)) {
+      item.selected = item.selected.filter((v: any) => v !== option);
+    } else {
+      item.selected.push(option);
+    }
+  }
+  selectedItems.value = props.itemList
+    .map((item: any) => {
+      return {
+        name: item.name,
+        selected: item.selected,
+      };
+    })
+    .filter((item: any) => {
+      if (Array.isArray(item.selected)) {
+        return item.selected.length > 0;
+      } else {
+        return item.selected;
+      }
+    });
+  that.$forceUpdate();
+};
+
+// 将所有选中的条件获取并显示在顶部
+// 动态获取所有选中的条件
+
+// 通过watchEffect监听itemList的变化，重新计算selectedItems的值
+
+// const selectedItems = computed(() => {
+//   // 这里会有一个问题，就是selectedItems的值不会更新，因为selectedItems是一个计算属性，只有在依赖的值发生变化时才会重新计算
+//   // 而因为selectedItems依赖的值是itemList，而itemList的值是不会发生变化的，所以selectedItems的值也不会发生变化
+//   // 所以这里需要使用watchEffect来监听itemList的变化，然后重新计算selectedItems的值
+//   const result = props.itemList
+//     .map((item: any) => {
+//       return {
+//         name: item.name,
+//         selected: item.selected,
+//       };
+//     })
+//     .filter((item: any) => {
+//       if (Array.isArray(item.selected)) {
+//         return item.selected.length > 0;
+//       } else {
+//         return item.selected;
+//       }
+//     });
+//   console.log(result);
+//   return result;
+// });
+
+// 监听itemList的变化
+// 当itemList发生变化时，重新计算selectedItems的值
+// watchEffect的用法是：watchEffect(effect: () => void, options?: WatchEffectOptions)
+// 例如：watchEffect(() => { console.log('effect') })
+// watchEffect(() => {
+//   selectedItems.value;
+// });
+
+// 逻辑处理
+// 1. 筛选条件
+// 2. 筛选结果
+// 3. 筛选结果展示
+// 4. 筛选结果操作
+// 5. 筛选结果分页
+</script>
+
+<style lang="scss" scoped>
+.active-filters-block {
+  @apply flex;
+  & > .__item {
+    @apply flex inline-block bg-white p-1 m-1 border border-gray-200 rounded items-center;
+  }
+}
+.closable-tag {
+  @apply inline-block  color-black p-1 m-1 border-dashed border-1 border-light;
+}
+.condition-block {
+  @apply p-1 bg-white border-t-1 border-dashed flex justify-between items-center;
+  // 第一个元素的上边框不显示
+  &:first-child {
+    @apply border-t-0;
+  }
+  // 奇数行为灰底，偶数行为白底
+  &:nth-child(odd) {
+    @apply bg-gray-1;
+  }
+  & > .__name {
+    @apply inline-block w-20;
+  }
+  .__item {
+    @apply inline-block cursor-pointer p-1 m-1 border border-gray-200 rounded;
+    // 单数行则为白底，双数行则为灰底
+    &:hover {
+      @apply bg-blue-3 color-black;
+    }
+    &.active {
+      @apply bg-blue-5 color-white;
+    }
+  }
+}
+</style>
