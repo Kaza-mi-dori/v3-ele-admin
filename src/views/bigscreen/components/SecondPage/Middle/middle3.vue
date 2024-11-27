b5403da031c9c399817c455b7a7893547bcf7f5c
weidongyun <weidongyun@gig.com>
2024-11-27 16:04:10 +0800

    提交大屏6个业态二级页面-中间3-经营收入分析-柱状图

--- src/views/bigscreen/components/SecondPage/Middle/middle3.vue
+++ src/views/bigscreen/components/SecondPage/Middle/middle3.vue
@@ -1,16 +1,137 @@
 <template>
   <Model1 class="model1" title="经营收入分析">
-    <div
-      id="chart2-middle-3"
-      style="height: 180px; width: 100%; margin-top: -10px"
-    />
+    <div id="chart2-middle-3" style="width: 100%; height: 230px; margin-top: -10px" />
   </Model1>
 </template>
 
 <script setup lang="ts">
 import Model1 from "../Model1/index.vue";
 import * as echarts from "echarts";
-import { ref, onMounted } from "vue";
+import { ref, onMounted, shallowRef } from "vue";
+
+// 每个类别对应的数据系列
+const months = [
+  "1月",
+  "2月",
+  "3月",
+  "4月",
+  "5月",
+  "6月",
+  "7月",
+  "8月",
+  "9月",
+  "10月",
+  "11月",
+  "12月",
+];
+
+// 随机生成收入数据
+const getRandomData = () => {
+  return months.map(() => ({
+    planned: Math.floor(Math.random() * 101), // 计划经营收入
+    actual: Math.floor(Math.random() * 101), // 实际经营收入
+  }));
+};
+
+const chart = shallowRef<echarts.ECharts | null>(null);
+
+const initChart = () => {
+  chart.value = echarts.init(
+    document.getElementById("chart2-middle-3") as HTMLDivElement
+  );
+  chart.value.clear();
+  const data = getRandomData();
+
+  const option = {
+    tooltip: {
+      trigger: "axis",
+      axisPointer: { type: "shadow" },
+    },
+    legend: {
+      data: ["计划经营收入", "实际经营收入"],
+      textStyle: {
+        color: "#5099E3",
+      },
+      top: 20,
+      right: 40,
+      itemWidth: 12,
+      itemHeight: 7,
+    },
+    grid: {
+      left: "3%",
+      right: "4%",
+      bottom: "2%",
+      containLabel: true,
+    },
+    xAxis: {
+      type: "category",
+      data: months,
+      axisLine: {
+        lineStyle: {
+          color: "#27518D",
+        },
+      },
+      axisLabel: {
+        fontSize: 15,
+        color: "#5099E3",
+      },
+    },
+    yAxis: {
+      type: "value",
+      axisLine: {
+        show: true, // 显示坐标轴线
+        lineStyle: {
+          color: "#27518D",
+        },
+      },
+      splitLine: {
+        show: true, // 显示分割线
+        lineStyle: {
+          type: "dashed", // 虚线
+          color: "#27518D",
+        },
+      },
+      axisLabel: {
+        fontSize: 16,
+        color: "#5099E3",
+      },
+    },
+    series: [
+      {
+        name: "计划经营收入",
+        type: "bar",
+        data: data.map((item) => item.planned),
+        itemStyle: {
+          color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+            { offset: 0, color: "#25DDA7" },
+            { offset: 1, color: "#249E55" },
+          ]),
+        },
+        barWidth: "25%",
+      },
+      {
+        name: "实际经营收入",
+        type: "bar",
+        data: data.map((item) => item.actual),
+        itemStyle: {
+          color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+            { offset: 0, color: "#f2b678" },
+            { offset: 1, color: "#f18c32" },
+          ]),
+        },
+        barWidth: "25%",
+      },
+    ],
+  };
+  chart.value.setOption(option);
+};
+
+onMounted(() => {
+  initChart();
+  window.addEventListener("resize", () => {
+    chart.value?.resize();
+  });
+});
 </script>
 
 <style lang="scss" scoped>
