d17dfdb3e28165c60ffb2bd0a2467acbfcd7c6c7
weidongyun <weidongyun@gig.com>
2024-11-28 11:36:05 +0800

    提交大屏6个二级页面-右2-结算交易分析-柱折混合

--- src/views/bigscreen/components/SecondPage/Right/right2.vue
+++ src/views/bigscreen/components/SecondPage/Right/right2.vue
@@ -1,11 +1,184 @@
 <template>
   <Model1 class="model1" title="结算交易分析">
-    <div />
+    <div class="risk-warning" @click="handleRiskWarningClick">
+      <span class="text">有</span>
+      <span class="risk-count">2</span>
+      <span class="text">份合同处于有履约风险状态</span>
+      <span>
+        <el-icon><ArrowRight /></el-icon>
+      </span>
+    </div>
+    <div
+      id="chart2-right-2"
+      style="width: 100%; height: 220px; margin-top: -30px"
+    />
   </Model1>
 </template>
 
 <script setup lang="ts">
 import Model1 from "../Model1/index.vue";
+import * as echarts from "echarts";
+import { ref, onMounted, shallowRef } from "vue";
+
+const chart = shallowRef<echarts.ECharts | null>(null);
+
+// 每个类别对应的数据系列
+const dates = ["10-01", "10-02", "10-03", "10-04", "10-05", "10-06", "10-07"];
+
+// 随机生成收入数据
+const getRandomData = () => {
+  return dates.map(() => ({
+    tradingVolume: Math.floor(Math.random() * 11), // 成交量
+    transactionVolume: Math.floor(Math.random() * 101), // 交易额
+  }));
+};
+
+const initChart = () => {
+  chart.value = echarts.init(
+    document.getElementById("chart2-right-2") as HTMLDivElement
+  );
+  chart.value.clear();
+  const data = getRandomData();
+
+  const option = {
+    tooltip: {
+      trigger: "axis",
+      axisPointer: {
+        type: "cross",
+        crossStyle: {
+          color: "#999",
+        },
+      },
+    },
+    grid: {
+      left: "3%",
+      right: "4%",
+      bottom: "2%",
+      containLabel: true,
+    },
+    legend: {
+      data: ["交易额", "成交量"],
+      textStyle: {
+        color: "#5099E3",
+      },
+      icon: "rect",
+      top: 10,
+      right: 40,
+      itemWidth: 12,
+      itemHeight: 7,
+    },
+    xAxis: [
+      {
+        type: "category",
+        data: dates,
+        axisLine: {
+          lineStyle: {
+            color: "#27518D",
+          },
+        },
+        axisLabel: {
+          fontSize: 15,
+          color: "#5099E3",
+        },
+        axisPointer: {
+          type: "shadow",
+        },
+      },
+    ],
+    yAxis: [
+      {
+        type: "value",
+        min: 0,
+        max: 100,
+        interval: 20,
+        axisLine: {
+          show: true, // 显示坐标轴线
+          lineStyle: {
+            color: "#27518D",
+          },
+        },
+        splitLine: {
+          show: true, // 显示分割线
+          lineStyle: {
+            type: "dashed", // 虚线
+            color: "#27518D",
+          },
+        },
+        axisLabel: {
+          fontSize: 16,
+          color: "#5099E3",
+        },
+      },
+      {
+        type: "value",
+        min: 0,
+        max: 10,
+        interval: 2,
+        axisLine: {
+          show: true, // 显示坐标轴线
+          lineStyle: {
+            color: "#27518D",
+          },
+        },
+        splitLine: {
+          show: true, // 显示分割线
+          lineStyle: {
+            type: "dashed", // 虚线
+            color: "#27518D",
+          },
+        },
+        axisLabel: {
+          fontSize: 16,
+          color: "#5099E3",
+        },
+      },
+    ],
+    series: [
+      {
+        name: "成交量",
+        type: "bar",
+        yAxisIndex: 1, // 指定使用右边的Y轴
+        data: data.map((item) => item.tradingVolume),
+        itemStyle: {
+          color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+            { offset: 0, color: "#18DBD4" },
+            { offset: 1, color: "#01B3F3" },
+          ]),
+        },
+        barWidth: "25%",
+      },
+      {
+        name: "交易额",
+        type: "line",
+        smooth: true, // 设置线条为圆滑
+        symbol: "none", // 不显示折线点的点号
+        lineStyle: {
+          color: "#E8325C", // 设置线条颜色
+          width: 3, // 线条宽度
+        },
+        areaStyle: {
+          color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+            { offset: 0, color: "rgba(232,50,92, 0.4)" }, // 折线图顶端颜色
+            { offset: 1, color: "rgba(219, 137, 67, 0)" }, // 底部渐变透明
+          ]),
+        },
+        data: data.map((item) => item.transactionVolume),
+      },
+    ],
+  };
+  chart.value.setOption(option);
+};
+
+const handleRiskWarningClick = () => {
+  console.log("风险提示信息被点击了");
+};
+
+onMounted(() => {
+  initChart();
+  window.addEventListener("resize", () => {
+    chart.value?.resize();
+  });
+});
 </script>
 
 <style lang="scss" scoped>
@@ -14,5 +187,38 @@ import Model1 from "../Model1/index.vue";
   flex: 1;
   flex-direction: column;
   height: 100%;
+}
+
+.risk-warning {
+  display: flex;
+  align-items: center;
+  font-size: 16px;
+  letter-spacing: 1px;
+  color: #fff;
+  cursor: pointer;
+  margin-top: 20px;
+  margin-left: 20px;
+  position: relative;
+  z-index: 10; /* 确保点击事件能触发 */
+  width: 55%; /* 避免遮挡右边图例导致点击不了 */
+}
+
+.risk-warning span {
+  display: inline-flex;
+  align-items: center;
+}
+
+.risk-warning .text {
+  color: #fff;
+}
+
+.risk-warning .risk-count {
+  color: #e8325c;
+  font-weight: bold;
+  margin: 0 5px;
+}
+
+.risk-warning:hover {
+  opacity: 0.7;
 }
 </style>
