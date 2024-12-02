46039d081a6f43701101875f82cc2811dc1e8bb7
weidongyun <weidongyun@gig.com>
2024-12-02 10:00:24 +0800

    优化代码

--- src/views/bigscreen/components/constants.ts
+++ src/views/bigscreen/components/constants.ts
@@ -23,11 +23,6 @@ export const businessTypes2: navItem[] = [
 ];
 
 export const allBusinessTypes: navItem[] = [
-  { id: 1, label: "LNG", name: "LNG" },
-  { id: 2, label: "燃料油", name: "fuelOil" },
-  { id: 3, label: "成品油", name: "refinedOilProduct" },
-  { id: 4, label: "化工产品", name: "chemicalProduct" },
-  { id: 5, label: "原油", name: "crudeOil" },
-  { id: 6, label: "煤炭", name: "coal" },
+  ...businessTypes,
   { id: 7, label: "市场信息", name: "marketInfo" },
 ];
