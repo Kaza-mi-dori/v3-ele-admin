7f0a0e1e4ca58880e0449fd48d79e5bea1aebc52
weidongyun <weidongyun@gig.com>
2024-12-17 17:56:10 +0800

    大屏首页-去掉页头左右两边的业态入口

--- src/views/bigscreen/index.vue
+++ src/views/bigscreen/index.vue
@@ -3,24 +3,8 @@
     <div v-if="showOtherContent" class="bg-view-img">
       <img style="position: absolute; top: 0" height="100vh" />
       <div class="bg-view__header">
-        <div class="header-left header-left-right">
-          <!-- 使用计算属性显示前一半元素 -->
-          <div v-for="item in businessTypeArrLeft" :key="item.id">
-            <div class="type-item" @click="handleType(item)">
-              {{ item.label }}
-            </div>
-          </div>
-        </div>
         <div class="title">
           <div class="__title--text">广投石化驾驶舱</div>
-        </div>
-        <div class="header-right header-left-right">
-          <!-- 使用计算属性显示后一半元素 -->
-          <div v-for="item in businessTypeArrRight" :key="item.id">
-            <div class="type-item" @click="handleType(item)">
-              {{ item.label }}
-            </div>
-          </div>
         </div>
       </div>
       <div class="bg-view__body">
@@ -35,34 +19,10 @@
 import ScreenIndexContent from "./index-content.vue";
 import { ref, computed } from "vue";
 import router from "@/router";
-import { businessTypes, navItem } from "./components/constants";
 import { businessStore, companyStore } from "@/store";
 
 const businessstore = businessStore();
 const companystore = companyStore();
-
-const businessTypeArr = ref<navItem[]>(businessTypes);
-
-const handleType = (item: any) => {
-  const businessName = item.name;
-  const route = router.resolve({
-    name: "Business",
-    params: { businessName },
-  });
-  window.open(route.href, "_blank");
-};
-
-// 计算属性，获取数组的前一半
-const businessTypeArrLeft = computed(() => {
-  const midIndex = Math.ceil(businessTypeArr.value.length / 2);
-  return businessTypeArr.value.slice(0, midIndex);
-});
-
-// 计算属性，获取数组的后一半
-const businessTypeArrRight = computed(() => {
-  const midIndex = Math.ceil(businessTypeArr.value.length / 2);
-  return businessTypeArr.value.slice(midIndex);
-});
 
 // 根据路由决定显示内容
 const showOtherContent = computed(() => {
@@ -173,29 +133,6 @@ onMounted(() => {
   height: 66px;
   display: flex;
   position: relative;
-  .header-left-right {
-    display: flex;
-    justify-content: center; /* 水平居中 */
-    align-items: center; /* 垂直居中 */
-    width: 30%;
-    height: 52px;
-  }
-  // .header-left {
-  //   display: flex;
-  //   justify-content: center; /* 水平居中 */
-  //   align-items: center; /* 垂直居中 */
-  //   position: absolute;
-  //   left: clamp(10px, 20%, 50px);
-  //   top: clamp(5px, 10px, 33px);
-  // }
-  // .header-right {
-  //   display: flex;
-  //   justify-content: center; /* 水平居中 */
-  //   align-items: center; /* 垂直居中 */
-  //   position: absolute;
-  //   right: clamp(10px, 20%, 50px);
-  //   top: clamp(5px, 10px, 33px);
-  // }
   .title {
     flex: 1;
     height: 66px;
@@ -223,23 +160,6 @@ onMounted(() => {
       -webkit-background-clip: text; /* 兼容性设置 */
       -webkit-text-fill-color: transparent; /* 兼容性设置 */
       position: relative;
-    }
-  }
-  .type-item {
-    background-image: url(./img/nav1.png);
-    background-repeat: no-repeat; /* 防止背景图片重复 */
-    background-size: cover; /* 背景图片填充整个元素，保持比例 */
-    width: 100px;
-    height: 42px;
-    margin-right: 15px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    color: #d6dde6;
-    font-size: 14px;
-    cursor: pointer;
-    &:hover {
-      background-image: url(./img/nav2.png);
     }
   }
 }
