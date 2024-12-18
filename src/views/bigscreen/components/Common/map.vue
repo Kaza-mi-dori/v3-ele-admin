f9384103e140257f2ef32b8a2cf1c276b75a2185
weidongyun <weidongyun@gig.com>
2024-12-18 14:48:23 +0800

    大屏首页-中间2地图-改为Vue组件库方式

--- src/views/bigscreen/components/Common/map.vue
+++ src/views/bigscreen/components/Common/map.vue
@@ -1,34 +1,64 @@
 <template>
-  <div class="map-wrapper">
-    <div id="mapContainer" class="map-container" />
-  </div>
+  <tlbs-map
+    ref="mapContainer"
+    api-key="4TYBZ-V2WCQ-IKX52-BEW3F-SDCA6-7DFMN"
+    :center="center"
+    :zoom="zoom"
+    :control="control"
+    :options="{
+      mapStyleId: styleId,
+    }"
+    class="map-container"
+    @click="onClick"
+  >
+    <tlbs-multi-marker
+      :geometries="geometries"
+      :styles="styles"
+      :options="options"
+    />
+  </tlbs-map>
 </template>
 
 <script setup lang="ts">
-import { onMounted } from "vue";
+import { ref } from "vue";
 
-const initMap = () => {
-  //定义地图中心点坐标
-  const center = new TMap.LatLng(35, 105);
-  //定义map变量，调用 TMap.Map() 构造函数创建地图
-  const map = new TMap.Map(document.getElementById("mapContainer"), {
-    center: center, // 设置地图中心点坐标
-    zoom: 2, //设置地图缩放级别
-    viewMode: "2D",
-    scaleControl: true, // 比例尺
-    mapStyleId: "style1", // 自定义样式
-  });
+const map = ref(null);
+// lat是纬度，lng是经度
+// 中国内陆中心点经纬度：lat: 35, lng: 105
+const center = ref({ lat: 35, lng: 105 });
+// 地图宽度、高度
+const size = ref({ width: 800, height: 600 });
+const zoom = ref(2);
+const onClick = (e: Event) => {
+  console.log(e);
+};
+const control = {
+  scale: {},
+  zoom: {
+    position: "bottomRight",
+  },
+};
+const geometries = [
+  { styleId: "marker", position: { lat: 39.91799, lng: 116.397027 } },
+];
+const styles = {
+  marker: {
+    width: 20,
+    height: 30,
+    anchor: { x: 10, y: 30 },
+  },
+};
+const options = {
+  minZoom: 1,
+  maxZoom: 15,
 };
 
-onMounted(() => {
-  initMap();
-});
+const styleId = "style1";
 </script>
 
 <style lang="scss" scoped>
 .map-container {
   width: 100%;
-  height: 100%;
   position: relative;
 }
 </style>
