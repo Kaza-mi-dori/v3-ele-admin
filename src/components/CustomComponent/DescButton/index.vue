c3717193c62ea361c921e8a5f1352e5da297dccf
jhx <jiahaoxie7@outlook.com>
2025-01-03 14:56:30 +0800

    增加带注释按钮

---
+++ src/components/CustomComponent/DescButton/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <el-popover placement="top" trigger="hover">
+    <template #default>
+      <div class="desc-button-content">
+        <div class="desc-button-content-title">{{ title }}</div>
+      </div>
+    </template>
+    <template #reference>
+      <el-button v-bind="attrs">{{ title }}</el-button>
+    </template>
+  </el-popover>
+</template>
+
+<script setup lang="ts">
+import { useAttrs } from "vue";
+const props = defineProps<{
+  title: string;
+  desc: string;
+}>();
+
+const attrs = useAttrs();
+</script>
+
+<style lang="scss" scoped>
+.desc-button-content {
+  text-align: center;
+}
+</style>
