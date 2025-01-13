bbbcffeb517d77674610bb14bff02d04c4fed296
jhx <jiahaoxie7@outlook.com>
2025-01-13 18:06:56 +0800

    增加业务实体搜索组件

---
+++ src/components/Business/Selector/contract.vue
@@ -0,0 +1,88 @@
+<template>
+  <el-select
+    :value="number"
+    filterable
+    remote
+    :remote-method="debouncedSearch"
+    placeholder="请输入合同编号选择合同"
+    @change="handleChange"
+  >
+    <el-option
+      v-for="item in options"
+      :key="item.value"
+      :label="item.label"
+      :value="item.value"
+    />
+  </el-select>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import BusinessStandbookAPI from "@/api/businessStandBook";
+import { useDebounceFn } from "@vueuse/core";
+
+const number = ref("");
+const options = ref<any>([]);
+const props = defineProps<{
+  value?: string;
+  prop: string;
+  propQueryInList?: boolean;
+}>();
+const emits: any = defineEmits(["input", "selected"]);
+
+watch(
+  () => props.value,
+  (newVal) => {
+    // TODO 根据props.prop查询合同确保有值
+    initValue(newVal);
+    emits("input", newVal);
+  }
+);
+
+const handleChange = (newVal: string) => {
+  emits("input", newVal);
+};
+
+const initValue = async (newVal: string | undefined) => {
+  if (!newVal) return;
+  // TODO 根据props.prop查询合同
+  try {
+    const res: any = await BusinessStandbookAPI.getContractLedgerRecordList({
+      [props.propQueryInList ? props.prop + "集合" : props.prop]:
+        props.propQueryInList ? [newVal] : newVal,
+      页码: 1,
+      页容量: 1,
+    });
+    const result = res["当前记录"].map((item: any) => {
+      return {
+        value: item[props.prop],
+        label: item["合同名称"],
+      };
+    });
+    options.value = result;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const queryContractSearch = async (query: string) => {
+  const res: any = await BusinessStandbookAPI.getContractLedgerRecordList({
+    [props.propQueryInList ? props.prop + "集合" : props.prop]:
+      props.propQueryInList ? [query] : query,
+    页码: 1,
+    页容量: 20,
+  });
+  const result = res["当前记录"].map((item: any) => {
+    return {
+      value: item[props.prop],
+      label:
+        item[props.prop] +
+        (item["合同名称"] ? " " + item["合同名称"] : "") +
+        (item["相对人名称"] ? "/" + item["相对人名称"] : ""),
+    };
+  });
+  options.value = result;
+};
+
+const debouncedSearch = useDebounceFn(queryContractSearch, 800);
+</script>
